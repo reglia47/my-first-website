6cd83acfd311c5cfd1db7129985832c7c6836adf
reglia47 <reglia4747@gmail.com>
2026-07-29 04:16:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function showMessage(){
     const message = document.getElementById("message");
+
     message.textContent = "恭喜！这个按钮是用 JavaScript 做出来的互动功能。";
 }
 
 function openGoogleTranslate() {
-    const currentUrl = window.location.href;
+    const currentUrl = "https://reglia47.github.io/my-first-website/";
     const googleTranslateUrl = 
     "https://translate.google.com/translate?sl=auto&tl=ja&u="
     + encodeURIComponent(currentUrl);
-    
-    window.open(googleTranslateUrl, "_blank");
+
+    window.open(googleTranslateUrl, "_self");
 }
